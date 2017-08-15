a2ba48f427c291c046f5f97fb53d6d33b87f2cec
Steve Chan <chanshunli@gmail.com>
2017-08-15 14:14:45 +0800

    所有的依赖都更新到最新

--- project.clj
+++ project.clj
@@ -3,24 +3,23 @@
   :url "https://github.com/clojuredatascience/ch7-recommender-systems"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.mahout/mahout-core "0.9" :exclusions [com.google.guava/guava]]
                  [org.apache.mahout/mahout-examples "0.9" :exclusions [com.google.guava/guava]]
-                 [gorillalabs/sparkling "1.2.2"]
+                 [gorillalabs/sparkling "2.1.2"]
                  [me.raynes/fs "1.4.6"]
-                 [medley "0.5.5"]
-                 [incanter "1.5.6"]
-                 [com.google.guava/guava "16.0"]
-                 [iota "1.1.2"]
-                 [medley "0.6.0"]]
+                 [medley "1.0.0"]
+                 [incanter "1.5.7"]
+                 [com.google.guava/guava "23.0"]
+                 [iota "1.1.3"]]
   :profiles {:dev
-             {:dependencies [[org.clojure/tools.cli "0.3.1"]]
+             {:dependencies [[org.clojure/tools.cli "0.3.5"]]
               :repl-options {:init-ns cljds.ch7.examples}
               :resource-paths ["data/ml-100k"]}
              :provided
              {:dependencies
-              [[org.apache.spark/spark-mllib_2.10 "1.1.0" :exclusions [com.google.guava/guava]]
-               [org.apache.spark/spark-core_2.10 "1.1.0" :exclusions [com.google.guava/guava com.thoughtworks.paranamer/paranamer]]]}}
+              [[org.apache.spark/spark-mllib_2.11 "2.2.0" :exclusions [com.google.guava/guava]]
+               [org.apache.spark/spark-core_2.11 "2.2.0" :exclusions [com.google.guava/guava com.thoughtworks.paranamer/paranamer]]]}}
   :main cljds.ch7.core
   :aot [sparkling.serialization sparkling.destructuring cljds.ch7.sparkling]
   :jvm-opts ["-Xmx4g"])
